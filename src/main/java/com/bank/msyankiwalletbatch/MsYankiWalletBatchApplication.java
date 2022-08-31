2e5807ef203e269353993ba7e53af2f7e16e3999
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-31 01:55:54 -0500

    add structure base

---
+++ src/main/java/com/bank/msyankiwalletbatch/MsYankiWalletBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msyankiwalletbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsYankiWalletBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsYankiWalletBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/msyankiwalletbatch/MsYankiWalletBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msyankiwalletbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsYankiWalletBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
